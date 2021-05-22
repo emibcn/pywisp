8a1e468c192925e485e45d65ff4218ecc1236eac
emibcn <emi@algorismia.com>
2021-05-22 17:49:38 +0200

    Change package name from `pywisp` to `pywisp-emibcn` because of name clash

--- setup.py
+++ setup.py
@@ -1,29 +1,39 @@
 
 from setuptools import setup, find_packages
 
-VERSION = (0, 0, 3)
+VERSION = (0, 0, 6)
 __version__ = '.'.join(map(str, VERSION))
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read().replace('(/', '(https://github.com/emibcn/pywisp/blob/master/')
+
 setup(
-    name = 'pywisp',
+    name = 'pywisp-emibcn',
     version = __version__,
     description = "Access AirControl2, AirOS & Mikrotik",
-    long_description = "Access AirControl2 (API), AirOS (SSH) & Mikrotik (SSH) from the same place",
-    author = "Emilio del Giorgio",
-    author_email = "https://github.com/emibcn",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = "https://github.com/emibcn",
+    author_email = "emibcn.github@gmail.com",
     url = "https://github.com/emibcn/pywisp",
+    project_urls={
+        "Bug Tracker": "https://github.com/emibcn/pywisp/issues",
+    },
     license = "GPLv3",
     platforms = ["any"],
-    packages = ['pywisp'],
+    packages = ['pywisp-emibcn'],
+    package_dir = {"": "."},
     py_modules=['pywisp', 'pywisp/mikrotik', 'pywisp/aircontrol', 'pywisp/sshdevice'],
     scripts = ["bin/pywisp"],
     install_requires = ['requests', 'paramiko', 'termcolor', "argparse", "configparser"],
     include_package_data = True,
-    classifiers = [
-        "Environment :: Command line",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GPLv3 License",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Environment :: Other Environment",
     ],
 )
